497e93df18a35332c8693751c516cc00c1a434c8
reihan-yr <r.yarjanlou@gmail.com>
2025-11-09 10:41:40 +0330

    chore: remove useSignup hook

--- hooks/useSignup.js
+++
@@ -1,7 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-
-export function useSignup() {
-  return useMutation({
-    mutationFn: (data) => signup(data.email, data.password, data.name),
-  });
-}
